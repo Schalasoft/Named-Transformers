74241651f5d056a57695826e214eb15e5882a4dd
Cian <47340380+Schalasoft@users.noreply.github.com>
2020-02-28 08:10:55 +0000

    Big/Small naming fix

--- NamedTransformers/NamedTransformers.cs
+++ NamedTransformers/NamedTransformers.cs
@@ -7,33 +7,29 @@ namespace NamedTransformers
     [HarmonyPatch(typeof(PowerTransformer), "OnSpawn")]
     public class PowerTransformerOnSpawn_Patch
     {
-        // Transformer name strings from the prefabs for localisation
-        public static string TransformerName = STRINGS.BUILDINGS.PREFABS.POWERTRANSFORMER.NAME;
-        public static string TransformerSmallName = STRINGS.BUILDINGS.PREFABS.POWERTRANSFORMERSMALL.NAME;
-
         public static void Postfix(PowerTransformer __instance)
         {
             SetTransformerName(__instance);
         }
 
         // Sets the name of the transformer
-        public static void SetTransformerName(PowerTransformer transformer)
+        private static void SetTransformerName(PowerTransformer transformer)
         {
             {
                 KSelectable selectable = transformer.GetComponent<KSelectable>();
                 if (selectable != null)
                 {
-                    string transformerName = selectable.name;
-                    {
-                        transformerName = TransformerName + " " + TransformerCounter.IncrementTransformerCount();
-                    }
+                    string transformerName = transformer.GetProperName();
+
+                    transformerName += " " + TransformerCounter.IncrementTransformerCount();
+
                     selectable.SetName(transformerName);
                 }
             }
         }
 
         // Class to handle numbering of the transformers
-        public static class TransformerCounter
+        private static class TransformerCounter
         {
             public static int TransformerCount = 1;
 
@@ -41,13 +37,6 @@ namespace NamedTransformers
             {
                 return TransformerCount++;
             }
-        }
-
-        
-        // Returns true if the transformer name passed in is for the big or small transformer
-        public static Boolean IsTransformer(string name)
-        {
-            return name.Equals(TransformerName) || name.Equals(TransformerSmallName);
         }
     }
 }
